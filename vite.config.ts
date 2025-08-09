46abc298fb2b14963782a832f6a44060b553f8fd
ramlakshman-org <ramlakshmanrishikesh@gmail.com>
2025-08-09 15:13:08 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
   root: "client",
   plugins: [react(), tsconfigPaths()],
   // publicDir is relative to root ("client")
@@ -14,6 +18,7 @@ export default defineConfig({
     emptyOutDir: true,
     assetsDir: "assets",
     sourcemap: true,
+    manifest: true,
     rollupOptions: {
       output: {
         assetFileNames: "assets/[name]-[hash][extname]",
@@ -25,4 +30,8 @@ export default defineConfig({
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-dom/client'],
   },
-});
+  base: env.VITE_APP_BASE_URL || '/',
+  define: {
+    'process.env': {}
+  }
+}});

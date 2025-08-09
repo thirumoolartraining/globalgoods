af39ab89ab812a5ee5034d6cacce2c4889a7dc12
ramlakshman-org <ramlakshmanrishikesh@gmail.com>
2025-08-09 16:44:40 +0530

    commit

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,6 @@
+console.log('POSTCSS CWD=', process.cwd());
+try { console.log('RESOLVE tailwindcss=', require.resolve('tailwindcss')); } catch {}
+
 module.exports = {
   plugins: {
     tailwindcss: {},
